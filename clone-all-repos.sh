399812c246381fd5df98328e946ac9cad30b6252
JP Bourget <jp@syncurity.net>
2019-09-05 20:30:49 -0400

    Update clone-all-repos.sh

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -2,6 +2,6 @@
 
 # Script which clones all the pack repos
 
-curl -s "https://api.github.com/users/StackStorm-Exchange/repos?per_page=100&page=1" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("stackstorm"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
-curl -s "https://api.github.com/users/StackStorm-Exchange/repos?per_page=100&page=2" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("stackstorm"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
-curl -s "https://api.github.com/users/StackStorm-Exchange/repos?per_page=100&page=3" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("stackstorm"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
+curl -s "https://api.github.com/users/syncurity-exchange/repos?per_page=100&page=1" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("syncurity"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
+curl -s "https://api.github.com/users/syncurity-exchange/repos?per_page=100&page=2" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("syncurity"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
+curl -s "https://api.github.com/users/syncurity-exchange/repos?per_page=100&page=3" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin):\n  if not  repo["name"].startswith("syncurity"):\n    continue\n  os.system("git clone " + repo["ssh_url"])'
